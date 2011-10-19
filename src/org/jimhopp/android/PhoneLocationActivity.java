25a177ae28d244293fc6e0e602143e02d89b9a74
Jim Hopp <jim.hopp@mylookout.com>
2011-10-19 08:16:12 -0700

    Display additional gps data. Add .gitignore.

--- src/org/jimhopp/android/PhoneLocationActivity.java
+++ src/org/jimhopp/android/PhoneLocationActivity.java
@@ -6,13 +6,46 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class PhoneLocationActivity extends Activity {
 	
-	//CurrentLocationModel model = new CurrentLocationModel();
-	//CurrentLocationView view;
+	TextView lat;
+	TextView lon;
+	TextView alt;
+	TextView accuracy;
+	TextView status;
+	TextView time;
+	TextView speed;
+	TextView provider;
+	
+	final String NO_LOC_DATA = "no location data";
+
+	
+	void updateView(Location loc) {
+		lat.setText(String.valueOf(loc.getLatitude()));
+        lon.setText(String.valueOf(loc.getLongitude()));
+        alt.setText(String.valueOf(loc.getAltitude()));
+        accuracy.setText(String.valueOf(loc.getAccuracy()));
+        speed.setText(String.valueOf(loc.getSpeed()));
+        provider.setText(loc.getProvider());
+        Time tm = new Time();
+    	tm.set(loc.getTime());
+    	time.setText(tm.format2445());
+	}
+	
+	void updateViewUnknown() {
+        lat.setText(NO_LOC_DATA);
+        lon.setText(NO_LOC_DATA);
+        alt.setText(NO_LOC_DATA);
+        accuracy.setText(NO_LOC_DATA);
+        speed.setText(NO_LOC_DATA);
+        provider.setText(NO_LOC_DATA);
+        time.setText(NO_LOC_DATA);
+        }
 	
     /** Called when the activity is first created. */
     @Override
@@ -20,46 +53,48 @@ public class PhoneLocationActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //view = new CurrentLocationView(this, model);
-        //model.start();
-        
-        final EditText lat = (EditText)findViewById(R.id.latitude);
-        final EditText lon = (EditText)findViewById(R.id.longitude);
-        
+        lat = (TextView)findViewById(R.id.latitude);
+        lon = (TextView)findViewById(R.id.longitude);
+        alt = (TextView)findViewById(R.id.altitude);
+        accuracy = (TextView)findViewById(R.id.accuracy);
+        status = (TextView)findViewById(R.id.status);
+        time = (TextView)findViewById(R.id.time);
+        speed = (TextView)findViewById(R.id.speed);
+        provider = (TextView)findViewById(R.id.provider);
+
+
+       
         LocationListener location = new LocationListener() {
 
 			@Override
 			public void onLocationChanged(Location location) {
-				// TODO Auto-generated method stub
-				lat.setText(String.valueOf(location.getLatitude()));
-				lon.setText(String.valueOf(location.getLongitude()));
+				updateView(location);
 			}
 
 			@Override
-			public void onProviderDisabled(String provider) {
-				// TODO Auto-generated method stub
-				
+			public void onProviderDisabled(String providerName) {
+				status.setText("Provider "+ providerName + " disabled");
 			}
 
 			@Override
-			public void onProviderEnabled(String provider) {
-				// TODO Auto-generated method stub
-				
+			public void onProviderEnabled(String providerName) {
+				status.setText("Provider "+ providerName + " enabled");
 			}
 
 			@Override
-			public void onStatusChanged(String provider, int status,
+			public void onStatusChanged(String providerName, int providerStatus,
 					Bundle extras) {
-				// TODO Auto-generated method stub
-				
+				status.setText("Provider "+ providerName + " has status " + providerStatus);
 			}
         	
         };
         
         LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         Location lastKnown = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        lat.setText(String.valueOf(lastKnown.getLatitude()));
-        lon.setText(String.valueOf(lastKnown.getLongitude()));
+
+        if (lastKnown != null) { updateView(lastKnown); }
+        else { updateViewUnknown(); }
+       
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000l, Float.valueOf("50.0"), location);
     }
 }
